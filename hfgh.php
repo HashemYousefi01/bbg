556f4e5d37ee38f3b17b90be5126899d8444eac7
HashemYousefi01 <138669481+HashemYousefi01@users.noreply.github.com>
2023-12-14 14:07:33 +0100

    Create hfgh.php

---
+++ hfgh.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+// Echo session variables that were set on previous page
+echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
+echo "Favorite animal is " . $_SESSION["favanimal"] . ".";
+?>
+
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+print_r($_SESSION);
+?>
