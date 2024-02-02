680fe349b88dcfe1bf1e910eea1b91de53dab4b4
HashemYousefi01 <138669481+HashemYousefi01@users.noreply.github.com>
2024-02-02 15:18:17 +0100

    Update hfgh.php

--- hfgh.php
+++ hfgh.php
@@ -7,7 +7,7 @@ session_start();
 
 <?php
 // Echo session variables that were set on previous page
-echo "Favorite color is bana " . $_SESSON["frww"] . ".<br>";
+echo "Favorite color is bana " . $_SESSON["frw"] . ".<br>";
 echo "Favorite animal is " . $_SESSION["fall"] . ".";
 echo "Favorite animal is " . $_SESSION["favnimal"] . "7.8925";
 ?>
