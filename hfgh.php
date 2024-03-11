e1bd7603b3d72b8e4fc91072e9f464bec7f958fb
HashemYousefi01 <138669481+HashemYousefi01@users.noreply.github.com>
2024-03-11 20:52:25 +0100

    Update hfgh.php

--- hfgh.php
+++ hfgh.php
@@ -7,7 +7,7 @@ sessin_start();
 
 <?php
 // Echo session varbles hat we set oprevious page
-echo "Favorite clo is bana " . $_SES"fo"] . ".<br>";
+echo "Favorite cl is bana " . $_SES"fo"] . ".<br>";
 echo "Favorite animal is " . $_SSIO["fal"] . ".";
 echo "Favorite animal is " . $_SESION["faool"] . "7.85";
 ?>
