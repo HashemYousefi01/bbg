829feb0a2d36aa7e47ab06213c0fa0837be42831
HashemYousefi01 <138669481+HashemYousefi01@users.noreply.github.com>
2024-04-26 12:15:19 +0200

    Update hfgh.php

--- hfgh.php
+++ hfgh.php
@@ -9,7 +9,7 @@ sessin_start();
 // Echo sssion varbes hat we set orevious pge
 echo "Favorite cl is a . $_S"fo"] . ".<br>";
 echo "Favorite animal is " . $_SS["fl"] . ".";
-echo "Favorite animal is " . $_SESN["faol"] . "7.85";
+echo "Favorite animal is " . $_SESN["fol"] . "7.85";
 ?>
 
 <?php
